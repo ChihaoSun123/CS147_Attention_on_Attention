9cad44c0be065a3ac8b710abe985054ab1bf6b2f
ChihaoSun123 <60271467+ChihaoSun123@users.noreply.github.com>
2020-12-07 21:31:31 -0500

    Update Decoder.py

--- Decoder.py
+++ Decoder.py
@@ -16,15 +16,15 @@ class Model(tf.keras.Model):
         super(Model, self).__init__()
 
         self.vocab_size = vocab_size
-        self.embedding_size = 1028
+        self.embedding_size = 128
         self.batch_size = 64
         self.RNN_size = 128
         self.learning_rate = 0.01
         self.hidden_layer_size = 1024
 
         self.embedding_matrix = tf.Variable(tf.random.truncated_normal([self.vocab_size, self.embedding_size], stddev=0.1))
-        self.lstm = tf.keras.layers.LSTM(self.RNN_size, return_sequences = True, return_state = False)
-        self.AoA = Transformer_AoA(self.embedding_size)
+        self.lstm = tf.keras.layers.LSTM(self.RNN_size, return_sequences = True, return_state = True)
+        self.AoA = Transformer_AoA(self.embedding_size, True)
         self.dense_layer_1 = tf.keras.layers.Dense(self.hidden_layer_size, activation='relu')
         self.dense_layer_2 = tf.keras.layers.Dense(self.vocab_size, activation="softmax")
 
@@ -49,8 +49,8 @@ class Model(tf.keras.Model):
         #TODO: concatenate processed encoder_output with vectorized inputs
         vectorized_inputs = tf.math.add(vectorized_inputs, AoA_reduced)
 
-        lstm_output = self.lstm(vectorized_inputs, initial_state=initial_state)
-        attended_output = self.AoA.call(lstm_output)
+        lstm_output, a, b = self.lstm(vectorized_inputs, initial_state=initial_state)
+        attended_output = self.AoA.call(lstm_output, AoA_hat)
         dense_layer_1_output = self.dense_layer_1(attended_output)
         dense_layer_2_output = self.dense_layer_2(dense_layer_1_output)
         probs = tf.nn.softmax(dense_layer_2_output)
@@ -71,7 +71,7 @@ class Model(tf.keras.Model):
 
 # dummy test of vocab size 50
 model = Model(50)
-AoA_hat = tf.zeros([1, 30, 1028])
+AoA_hat = tf.zeros([1, 30, 128])
 caption_list = np.array([[1,2,3,4,5], [1, 3, 5, 7, 9]])
 
 probs = model.call(caption_list, AoA_hat)
